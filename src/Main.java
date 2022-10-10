98185f1ed3126b8d952ce4f02c34d10500209713
fhvfn94 <fhvfn94@gmail.com>
2022-10-11 00:35:30 +0300

    add task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1(){
+        int result = 0;
+        int salary = 15000;
+        int i = 0;
+        int pais = 0;
+        while (result <= 2459000){
+            pais = result / 100;
+            result = result + salary + pais;
+            i++;
+            System.out.println("месяц " + i + " сумма накоплений равна " + result + "рублей");
+        }
     }
 }
